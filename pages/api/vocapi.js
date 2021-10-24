4b8e929a8b40841fd553d21ea7d014b8b924848e
pierre <pierre_mozin@hotmail.com>
2021-10-24 21:02:45 +0200

    modif path suite deploy vercel

--- pages/api/vocapi.js
+++ pages/api/vocapi.js
@@ -2,17 +2,17 @@ import fs from "fs";
 import path from "path";
 
 export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), "data", "listes.json");
+  const filePath = path.join("cours-nextjs.vercel.app", "data", "listes.json");
   const fileData = fs.readFileSync(filePath);
   const data = JSON.parse(fileData);
 
   if (req.method === "GET") {
     res.status(200).json(data);
   } else if (req.method === "POST") {
-    const enWord = req.body.en;
-    const frWord = req.body.fr;
+    //const enWord = req.body.en;
+    //const frWord = req.body.fr;
 
-    const newWord = { en: enWord, fr: frWord };
+    const newWord = req.body;
     data.englishList[0].data.push(newWord);
     fs.writeFileSync(filePath, JSON.stringify(data));
     res.status(201).json({ message: "Succès!" });
